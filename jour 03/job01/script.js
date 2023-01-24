f399b64168e6456a9f74d3b7e4944bf47a9cc842
maleye rane <maleyeranendoye@Mydebian.lan>
2023-01-24 14:21:57 +0100

    jquery job01

---
+++ jour 03/job01/script.js
@@ -0,0 +1,7 @@
+function myFuction(){
+    document.getElementById("demo").innerHTML = "Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,ensuite on prie.";
+}
+
+function myFuctionhide(){
+    document.getElementById("demo").style.display = "none";
+}
